471ec0ea7d7a243e4136212453ea66550f90739c
Santiago Torres G <thdealer@gmail.com>
2020-04-28 16:37:22 -0500

    Add random line selector

--- lib/tweeterer/file_reader.ex
+++ lib/tweeterer/file_reader.ex
@@ -3,6 +3,7 @@ defmodule Tweeterer.FileReader do
     File.read!(path)
     |> String.split("\n")
     |> Enum.map(&String.trim/1)
-    |> Enum.filter(&String.length(&1) <= 140)
+    |> Enum.filter(&String.length(&1) <= 280)
+    |> Enum.random()
   end
 end
